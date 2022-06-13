847b10c69eaa6d512eecc8939fb39909abc7f717
tim <tjhickey@brandeis.edu>
2022-06-13 09:42:39 -0400

    updated counter

--- firstproject/App.js
+++ firstproject/App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-//import Grading from './components/grading';
-import Grading from './components/exam1Ans'
+
+import Counter from './components/counter';
 
 export default function App() {
   return (
-    <Grading />
+    <Counter />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ firstproject/components/counter.js
@@ -0,0 +1,27 @@
+import React,{useState} from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+
+const Counter = () => {
+    const [count,setCount] = useState(0);
+    return (
+        <View style={styles.container}>
+        <Text>The counter value is {count} </Text>
+        <Button 
+            title="push" 
+            onPress = {() => setCount(count+1)}
+            
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    backgroundColor: '#ecf0f1',
+    padding: 8, 
+  },
+ 
+});
+export default Counter;
