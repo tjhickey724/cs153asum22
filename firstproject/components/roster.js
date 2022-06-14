c33ba0e4e9df1c92dab5da9cb2cf5d085b953565
tim <tjhickey@brandeis.edu>
2022-06-14 09:39:30 -0400

    fixed roster code

--- firstproject/components/roster.js
+++ firstproject/components/roster.js
@@ -1,34 +1,58 @@
 import React from 'react';
-import { Text,  View } from 'react-native';
+import { Text,  FlatList, View, StyleSheet } from 'react-native';
 
-const data = 
-[
-    {key:'Tim', email:'tjhickey@brandeis.edu', location:'Brookline, MA, USA',age:66,},
-    {key:'Anthony',email:'anthonyquinn@brandeis.edu',location:'Norwood,MA,USA',age: 21,},
-    {key:'Daniel', email:'danielmints@brandeis.edu', location:'Boston, MA, USA', age:21,},
-    {key:'Harry', email:'harryyu@brandeis.edu', location:'Waltham, MA, USA', age:19,},
-    {key: 'Oriana', email:'sihechen@brandeis.edu', location:'Albany, NY, USA', age: '21', },
-    {key: 'Alicia', email: 'tujy@brandeis.edu', location: 'Waltham, MA, USA', age: 21},
-    {key:'Allison', email:'allisonchanin@brandeis.edu',location:'Palo Alto, CA', age: 18,},
-    {key:'Karen', email: 'karenmai@braneis.edu', location: 'Boston, MA', age: '19'},
-    {key:'Rex Xing', email:'qishixing@brandeis.edu', location:'Waltham, MA, USA', age:22},
-    {key:'Nathan', email: 'nathanieltran@brandeis.edu', location: 'Newton, MA', age: 20,},
-    {key: 'Ephraim', email: 'ezimmerman@brandeis.edu', location: 'South Haven, MI', age: '19'},
-    {key:'Rotem', email: 'rotemarie@brandeis.edu', location:'Israel', age: '23'},
-    {key:'Maggie', email:'maggiechang@brandeis.edu', location:'Irvine, CA, USA',age:'20',},
-    {key:'Eric',email:'erichu@brandeis.edu',location:'Seattle,WA',age:'20'},
-    {key:'Aria', email:'ariasmith@brandeis.edu', location:'Ellicott City,MD, USA', age: '20',},,
-   
-    ];
-    
+
+const styles = StyleSheet.create({
+    container: {
+     flex: 1,
+     paddingTop: 22
+    },
+    item: {
+      padding: 10,
+      fontSize: 18,
+      height: 44,
+    },
+  });
+
+  const data =
+  [
+    { key: 'Tim', email: 'tjhickey@brandeis.edu', location: 'Brookline, MA, USA', age: '66' },
+    { key: 'Anthony', email: 'anthonyquinn@brandeis.edu', location: 'Norwood,MA,USA', age: '21' },
+    { key: 'Daniel', email: 'danielmints@brandeis.edu', location: 'Boston, MA, USA', age: '21' },
+    { key: 'Harry', email: 'harryyu@brandeis.edu', location: 'Waltham, MA, USA', age: '19', },
+    { key: 'Oriana', email: 'sihechen@brandeis.edu', location: 'Albany, NY, USA', age: '21' },
+    { key: 'Alicia', email: 'tujy@brandeis.edu', location: 'Waltham, MA, USA', age: '21' },
+    { key: 'Allison', email: 'allisonchanin@brandeis.edu', location: 'Palo Alto, CA', age: '18' },
+    { key: 'Karen', email: 'karenmai@braneis.edu', location: 'Boston, MA', age: '19' },
+    { key: 'Rex Xing', email: 'qishixing@brandeis.edu', location: 'Waltham, MA, USA', age: '22' },
+    { key: 'Nathan', email: 'nathanieltran@brandeis.edu', location: 'Newton, MA', age: '20' },
+    { key: 'Ephraim', email: 'ezimmerman@brandeis.edu', location: 'South Haven, MI', age: '19' },
+    { key: 'Rotem', email: 'rotemarie@brandeis.edu', location: 'Israel', age: '23' },
+    { key: 'Maggie', email: 'maggiechang@brandeis.edu', location: 'Irvine, CA, USA', age: '20' },
+    { key: 'Eric', email: 'erichu@brandeis.edu', location: 'Seattle,WA', age: '20' },
+    { key: 'Aria', email: 'ariasmith@brandeis.edu', location: 'Ellicott City,MD, USA', age: '20' },
+ ];
+
+ const rosterItem = (item) => {
+    return (
+    <View>
+        <Text>{item.key}</Text>
+        <Text>{item.email}</Text>
+    </View>
+    );
+ }
+ 
+ 
+
 const Roster = () => {
     return (
         <View>
             <Text>Roster</Text>
-            <Text>
-                {JSON.stringify(data,null, 5)}
-            </Text>
-
+            <FlatList
+               data = {data}
+               renderItem={({item}) => rosterItem(item)}
+            /> 
+            <Text> {JSON.stringify(data,null,5)}</Text>
         </View>
     )
 }
