b669351074289c235ffb33f1cbeefce7507707bd
tim <tjhickey@brandeis.edu>
2022-06-15 09:49:29 -0400

    first APIdemo working

--- firstproject/components/APIdemo.js
+++ firstproject/components/APIdemo.js
@@ -1,22 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Text, TextInput, View } from 'react-native';
 
-const getMoviesFromApiAsync = async () => {
-    try {
-      const response = await fetch(
-        'https://reactnative.dev/movies.json'
-      );
-      const json = await response.json();
-      return json.movies;
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  
+
+
 const APIdemo = () => {
+    const [data,setData] = useState("");
+    const [loading,setLoading] = useState(true);
+
+    const getMovies = async () => {
+        try {
+          const response = await fetch('https://reactnative.dev/movies.json');
+          const json = await response.json();
+          setData(json.movies); 
+        } catch (error) {
+          console.error(error);
+        } finally {
+            setLoading(false);
+        }
+      };
+
+    useEffect(() => {getMovies()}, [])
+
     return(
         <View>
             <Text>API Demo</Text>
+            <Text>{JSON.stringify(data)}</Text>
         </View>
     );
 }
