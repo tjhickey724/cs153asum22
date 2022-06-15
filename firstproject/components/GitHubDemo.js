3401a96b0bfc1777496c1e50493ea43df43b94fc
tim <tjhickey@brandeis.edu>
2022-06-15 10:43:07 -0400

    copied in nice githubdemo

--- firstproject/components/GitHubDemo.js
+++ firstproject/components/GitHubDemo.js
@@ -1,42 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import { Text, FlatList, View } from 'react-native';
-
+import React, { useEffect, useState } from 'react';
+import { Text, View, TextInput, ActivityIndicator, FlatList, Button } from 'react-native';
 
 
 const GitHubDemo = () => {
-    const [data,setData] = useState([]);
-    const [loading,setLoading] = useState(true);
-    const [username,setUsername] = useState("tjhickey724")
+
+    const [data, setData] = useState([])
+    const [isLoading, setLoading] = useState(true);
+    const [temporaryUsername, setTemporaryUsername] = useState('tjhickey724');
+
+    const [username, setUsername] = useState('tjhickey724');
 
     const getRepos = async () => {
         try {
-          const response = await fetch('https://api.github.com/users/tjhickey724/repos');
-          const json = await response.json();
-          setData(json); 
+            const response = await fetch('https://api.github.com/users/' + username + '/repos');
+            const json = await response.json();
+            setData(json);
         } catch (error) {
-          console.error(error);
+            console.error(error);
         } finally {
             setLoading(false);
         }
-      };
+    }
 
-    useEffect(() => {getRepos()}, [username])
+    useEffect(() => { getRepos() }, [username])
 
-    return(
-        <View>
-            <Text>Github Demo</Text>
-            
-            <FlatList
-                data={data}
-                keyExtractor={({ id }, index) => id}
-                renderItem={({ item }) => (
-                    <Text>{item.name}</Text>
-          )}
-        />
-           
+    return (
 
+        <View style={{ flex: 1, padding: 24 }}>
+
+            <TextInput
+                style={{ height: 40 }}
+                placeholder="Enter GitHub Username"
+                onChangeText={newText => setTemporaryUsername(newText)}
+                defaultValue={temporaryUsername}
+            />
+
+            <Button
+                title="Search"
+                onPress={() => {
+                    setUsername(temporaryUsername)
+                }}
+            />
+
+            {/* <Text>{JSON.stringify(data)}</Text> */}
+
+            {isLoading ? <ActivityIndicator /> : (
+                <FlatList
+                    data={data}
+                    keyExtractor={({ id }, index) => id}
+                    renderItem={({ item }) => (
+                        <Text>{item.name}</Text>
+
+                    )}
+                />
+            )}
         </View>
-    );
+    )
 }
 
-export default GitHubDemo;
+export default GitHubDemo
