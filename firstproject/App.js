e9df39616744a6cd045fb14eaafd3fc95e85eec6
tim <tjhickey@brandeis.edu>
2022-06-15 08:54:25 -0400

    added Pomodoro example

--- firstproject/App.js
+++ firstproject/App.js
@@ -4,11 +4,12 @@ import React from 'react';
 import Grading from './components/grading';
 import Exam2a from './components/exam2a'
 import Exam2b from './components/exam2b'
+import Pomodoros from './components/Pomodoros'
 
 //import BMI from './components/bmi'
 export default function App() {
   return (
-    <Exam2b />
+    <Pomodoros />
   );
 }
 
