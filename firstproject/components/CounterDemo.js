2b7ae89ab92692f0e382b9909cc8e538399c299b
tim <tjhickey@brandeis.edu>
2022-06-21 10:11:45 -0400

    updated NamedCounter code

--- firstproject/components/CounterDemo.js
+++ firstproject/components/CounterDemo.js
@@ -1,24 +1,27 @@
 import React,{useState} from 'react';
 import {View,Text} from 'react-native';
-
 import NamedCounter from './NamedCounter';
 
-
-export default function App() {
-    const [total,setTotal] = useState(0);
-    const updateTotal = (v) => setTotal(v+total);
-  return (
-    <View>
-      <Text>New Named Counter Demo</Text>
-      <Text> total value is {total} cents</Text>
-      <View style={{flexDirection:'row'}}>
-        <NamedCounter  label="penny" value={1} updateTotal={updateTotal}/>
-        <NamedCounter  label="nickel" value={5} updateTotal={updateTotal}/>
-        <NamedCounter  label="dime" value={10} updateTotal={updateTotal}/>
-        <NamedCounter  label="quarter" value={25} updateTotal={updateTotal}/>
+export default function CounterDemo() {
+  
+    const [total, setTotal] = useState(0)
+    const [totalCoins, setTotalCoins] = useState(0)
+  
+    const updateTotal = (v) => {setTotal(v + total); setTotalCoins(totalCoins + 1)};
+  
+  
+    return (
+      <View>
+        <Text>Total value is ${total / 100} </Text>
+        <Text>Total amount of coins are {totalCoins} </Text>
+  
+        <View style={{ flexDirection: "row" }}>
+          <NamedCounter label="Penny" value={1} updateTotal={updateTotal} />
+          <NamedCounter label="Nickel" value={5} updateTotal={updateTotal}/>
+          <NamedCounter label="Dime" value={10} updateTotal={updateTotal} />
+          <NamedCounter label="Quarter" value={25} updateTotal={updateTotal} />
+        </View>
       </View>
-    </View>
-    
-  );
-}
-
+  
+    );
+  }
--- firstproject/components/NamedCounter.js
+++ firstproject/components/NamedCounter.js
@@ -1,36 +1,42 @@
-import React,{useState} from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Button, AsyncStorage } from 'react-native';
 
-const NamedCounter = ({label,value,updateTotal}) => {
-    /* increase the count by "value" when it pushed
-       and display the label in the view ..
-    */
-    const [count,setCount] = useState(0);
-    const [coins,setCoins] = useState(0);
-    return (
-        <View style={styles.container}>
-            <Text>{coins} coins</Text>
-            <Text>{count} cents</Text>
-            <Button 
-                color = '#FAD4D4'
-                title={label} 
-                onPress = {() => {
-                    updateTotal(value);
-                    setCount(count+value);
-                    setCoins(coins+1);
-                    }
-                }
-                />
-        </View>
-    );
+const NamedCounter = ({ label, value, updateTotal }) => {
+
+  const [count, setCount] = useState(0);
+  const [coins, setCoins] = useState(0);
+
+  return (
+    <View style={styles.container}>
+
+      <Text>The coin count is {coins}, </Text>
+      <Text>The counter value is ${count / 100} </Text>
+      <View style={styles.buttonView}>
+        <Button
+          title={label}
+          color="blue"
+          onPress={() => {
+            setCount((count + value));
+            updateTotal(value);
+            setCoins(coins + 1);
+            }
+          }
+        />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'flex-start',
-    padding: 8, 
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
- 
+  buttonView: {
+    flexDirection: "row"
+  }
+
 });
 export default NamedCounter;
