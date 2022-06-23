4597e2d2482832c8378075feff34191d99003b5b
tim <tjhickey@brandeis.edu>
2022-06-23 10:02:24 -0400

    more changes to asyncdemo

--- firstproject/components/Async1.js
+++ firstproject/components/Async1.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import {View,Text,TextInput} from 'react-native';
-import {useValue} from './ValueContext'
+import React,{useState} from 'react';
+import {View,Text,TextInput,Button} from 'react-native';
+import {useValue} from './ValueContext';
 
 const Async1 = () => {
+    const [text,setText] = useState("");
     const {currentValue,setCurrentValue} = useValue();
     return (
         <View>
             <Text> Async1 </Text>
             <Text> username: {currentValue.username} </Text>
+            <View style={{flexDirection:'row'}}>
+                <Text>Update your username:</Text>
+                <TextInput
+                    style={{backgroundColor:'yellow'}}
+                    onChangeText = {(text)=>setText(text)} 
+                />
+            </View>
+            
+            <Button 
+               title="save username"
+               onPress = {() => setCurrentValue({username:text})}
+               />
             
         </View>
     )
